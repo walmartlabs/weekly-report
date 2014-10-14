af0b167ddb63870f9dfb544e8c9cef41624d52f9
Craig Beck <luckybonza@gmail.com>
2014-10-13 22:41:34 -0700

    add empty shell of project

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+/**
+ * Webpack configuration
+ */
+
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+  cache: true,
+  context: path.join(__dirname, "app"),
+  entry: "./js/app/app.jsx",
+  output: {
+    path: path.join(__dirname, "app/js-dist"),
+    filename: "bundle.js"
+  },
+  optimize: {
+    minimize: true
+  },
+  module: {
+    loaders: [
+      { test: /\.jsx$/, loader: "jsx-loader" },
+      { test: /\.hbs$/, loader: "handlebars-loader" }
+    ]
+  },
+  resolve: {
+    alias: {
+      "underscore": "lodash/dist/lodash.underscore"
+    }
+  },
+  plugins: [
+    // Globals.
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+    // Manually do source maps to use alternate host.
+    new webpack.SourceMapDevToolPlugin(
+      "bundle.js.map",
+      "\n//# sourceMappingURL=http://127.0.0.1:3001/app/js-dist/[url]")
+  ]
+};
