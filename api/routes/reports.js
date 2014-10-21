6b4e350b525150bc87a1a6bd31b9e0c6846613f9
Kevin Stephens <kevinmstephens@gmail.com>
2014-10-21 11:24:03 -0700

    project setup again

--- api/routes/reports.js
+++ api/routes/reports.js
@@ -1,18 +1,5 @@
 
-
-
 module.exports = function (server) {
-
-  // Create new survey
-  server.route({
-    method: "POST",
-    path: "/surveys/create",
-    handler: function (req, res) {
-
-      global.console.log(req.body);
-      res("HELLO WORLD!");
-    }
-  });
 
   // Get existing survey
   server.route({
