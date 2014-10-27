bdd68c12d936507be63aef85804ed2837cd99a9b
Kevin Stephens <kevinmstephens@gmail.com>
2014-10-27 09:43:27 -0700

    refactor test

--- test/api/routes/reports.spec.js
+++ test/api/routes/reports.spec.js
@@ -13,6 +13,21 @@ var stdizer = function (object) {
 };
 
 describe("api/routes/reports", function () {
+  var server;
+
+  before(function (done) {
+    startServer(null, function (serverRef) {
+      server = serverRef;
+      done();
+    });
+  });
+
+  after(function (done) {
+    server.stop(function () {
+      done();
+    });
+  });
+
   var testSurvey = {
     periodStart: moment("20140101", "YYYYMMDD").toISOString(),
     periodEnd: moment("20140115", "YYYYMMDD").toISOString(),
@@ -21,67 +36,37 @@ describe("api/routes/reports", function () {
     emails: ["hi@gmail.com", "lo@gmail.com"]
   };
 
-  var apiRes;
-  var resBody;
-  var survey;
-  var responses;
-  var serverRef;
-
-  after(function (done) {
-    serverRef.stop(function () {
-      done();
-    });
-  });
-
-  before(function (done) {
-    startServer(null, function (server) {
-      serverRef = server;
-
-      server.inject({
-        method: "POST",
-        url: "/surveys",
-        payload: testSurvey
-      }, function (res) {
-        apiRes = res;
-        resBody = JSON.parse(res.payload);
-        // Go to db and get record
-        var models = server.plugins.sqlModels.models;
-        models.Survey.find({
-          where: { projectName: testSurvey.projectName }
+  it("should populate records and respond with servey record", function (done) {
+    server.inject({
+      method: "POST",
+      url: "/surveys",
+      payload: testSurvey
+    }, function (res) {
+      var resBody = JSON.parse(res.payload);
+      // Go to db and get record
+      var models = server.plugins.sqlModels.models;
+      models.Survey.find({
+        where: { projectName: testSurvey.projectName }
+      })
+      .then(function (survey) {
+        models.Response.findAll({
+          where: { SurveyId: survey.dataValues.id }
         })
-        .then(function (surveyRecord) {
-          survey = surveyRecord;
-          models.Response.findAll({
-            where: { SurveyId: surveyRecord.dataValues.id }
-          })
-          .then(function (responseRecords) {
-            responses = responseRecords;
-            done();
+        .then(function (response) {
+
+          test.done(done, function () {
+            expect(res.statusCode).to.equal(200);
+            expect(stdizer(resBody.newSurvey))
+              .to.deep.equal(_.omit(testSurvey, "emails"));
+            expect(stdizer(survey.dataValues))
+              .to.deep.equal(_.omit(testSurvey, "emails"));
+            expect(response[0].dataValues.email)
+              .to.equal(testSurvey.emails[0]);
+            expect(response[1].dataValues.email)
+              .to.equal(testSurvey.emails[1]);
           });
         });
       });
-    });
-  });
-
-  it("should respond with newly created survey object", function (done) {
-    test.done(done, function () {
-      expect(apiRes.statusCode).to.equal(200);
-      expect(stdizer(resBody.newSurvey))
-        .to.deep.equal(_.omit(testSurvey, "emails"));
-    });
-  });
-
-  it("should find survey in database", function (done) {
-    test.done(done, function () {
-      expect(stdizer(survey.dataValues))
-        .to.deep.equal(_.omit(testSurvey, "emails"));
-    });
-  });
-
-  it("should find related responses in database", function (done) {
-    test.done(done, function () {
-      expect(responses[0].dataValues.email).to.equal(testSurvey.emails[0]);
-      expect(responses[1].dataValues.email).to.equal(testSurvey.emails[1]);
     });
   });
 });
