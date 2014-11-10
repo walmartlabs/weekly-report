deda9eaa03ca690b8a3b87702fb580c536b12c85
Angela Nicholas <anicholas@walmartlabs.com>
2014-11-10 13:34:27 -0800

    add joi

--- api/lib/utils.js
+++ api/lib/utils.js
@@ -16,7 +16,7 @@ var _ = require("lodash");
 var Chance = require("chance");
 var chance = new Chance();
 var when = require("when");
-var moment = require("moment");
+var Joi = require("joi");
 
 // For use in .catch.
 // Log error and exit process
@@ -174,7 +174,8 @@ var validArrayJSON = function (options) {
  * @return {Boolean}      True if date is formatted "YYYYMMDD"
  */
 var isYYYYMMDD = function (date) {
-  return moment(date, "YYYYMMDD", true).isValid();
+  var schema = Joi.date().format("YYYYMMDD");
+  return !schema.validate(date).error;
 };
 
 module.exports = {
