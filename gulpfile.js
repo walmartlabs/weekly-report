97441f8a6d1e5899ae885a484406e625828c0f1d
Kevin Stephens <kevinmstephens@gmail.com>
2014-11-17 18:31:12 -0800

    remove copy tasks from gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -45,18 +45,6 @@ gulp.task("open-browser", function () {
     options.port, "dev", "responses"));
 });
 
-gulp.task("copy", function () {
-  gulp.src("./app/assets/**/*")
-    .pipe(gulp.dest("./build/assets"));
-  gulp.src("./node_modules/font-awesome/css/**/*")
-    .pipe(gulp.dest("./build/assets/fonts/css"));
-  gulp.src("./node_modules/font-awesome/fonts/**/*")
-    .pipe(gulp.dest("./build/assets/fonts/fonts"));
-
-  return;
-});
-
-
 var runCss = function () {
   return gulp.src(options.scssPath + "main.scss")
     .pipe(sass({
@@ -232,7 +220,7 @@ gulp.task("server:dev", function () {
 // Aggregated Tasks
 // ----------------------------------------------------------------------------
 gulp.task("build", function () {
-  return runSequence("clean", ["js", "css", "copy"]);
+  return runSequence("clean", ["js", "css"]);
 });
 
 gulp.task("start", function () {
